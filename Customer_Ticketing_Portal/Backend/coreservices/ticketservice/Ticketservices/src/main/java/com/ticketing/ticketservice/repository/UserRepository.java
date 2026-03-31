c9b332686733e411b72077b24ce8bde75c6e63fd
ayan-coder323 <ayan38540@gmail.com>
2026-03-31 10:52:18 +0530

    Removed Customer Ticketing and Inventory Dashboard projects

--- Customer_Ticketing_Portal/Backend/coreservices/ticketservice/Ticketservices/src/main/java/com/ticketing/ticketservice/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ticketing.ticketservice.repository;
-
-import com.ticketing.ticketservice.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Long> {
-
-}
--- Inventory_Dashboard_Manager/Backend/gateway/run.py
+++
@@ -1,5 +0,0 @@
-import uvicorn
-from main import app
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8001, reload=True)
