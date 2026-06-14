dd3bfda9cd6537ec5930885b3c7b2af3e7408424
ayan-coder323 <ayan38540@gmail.com>
2026-06-15 01:17:20 +0530

    Remove unwanted folders from repository

--- Mid_Sem_Exam/Backend/coreservices/Exam/src/main/java/mth/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package mth.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
